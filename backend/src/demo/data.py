f75dddd0323abe2c326d31df30ac74aa799132e8
Anshul Chauhan <anshul.chauhan@nelkinda.com>
2021-12-27 16:14:10 +0530

    Test commit to fix relative import issue

---
+++ backend/src/demo/data.py
@@ -0,0 +1,18 @@
+from backend.models import Title, database
+
+
+@database.transaction()
+async def load_fixture():
+    await Title.objects.create(ident="wikipedia_en_test")
+    await Title.objects.create(ident="wikipedia_hi_test")
+    await Title.objects.create(ident="wikipedia_zh_test")
+    await Title.objects.create(ident="wikipedia_et_test")
+    await Title.objects.create(ident="wikipedia_fr_test")
+    await Title.objects.create(ident="wikipedia_de_test")
+    await Title.objects.create(ident="wikipedia_gu_test")
+    await Title.objects.create(ident="wikipedia_lt_test")
+    await Title.objects.create(ident="wikipedia_ml_test")
+    await Title.objects.create(ident="wikipedia_no_test")
+
+
+load_fixture()
