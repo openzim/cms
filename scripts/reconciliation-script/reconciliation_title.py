9cf980e16185b294f25127555a1d6793b0224081
Fadiga Ibrahima <ibfadiga@gmail.com>
2022-03-21 13:12:02 +0000

    Moving the reconciliation_title and improving variable names

--- scripts/reconciliation-script/reconciliation_title.py
+++ scripts/reconciliation-script/reconciliation_title.py
@@ -1,7 +1,10 @@
 import csv
+import re
 from xml.dom.minidom import parseString
 
 import requests
+from cli_formatter.output_formatting import info
+from cli_formatter.table_builder import TableBuilderClassic
 from zimscraperlib.i18n import NotFound, get_language_details
 
 
@@ -12,23 +15,25 @@ class ReconciliationTitle:
         self.url_api_zimfarm = url_api_zimfarm
         self.url_library_zim = url_library_zim
 
-        self.ident_not_in_library_names = []
-        self.ident_in_zimfarm = []
-        self.library_names_not_proper = []
+        self.ident_from_name_not_found_in_zimfarm = []
+        self.ident_from_name_found_in_zimfarm = []
+        self.books_names_not_proper = []
         self.code_languages_not_found = []
         self.data_ident = []
-        self.zimfarm_names_not_in_library = []
+        self.name_reg = re.compile(
+            r"^(?P<project>[a-z\-\.]+)_(?P<lang_codes>[a-z\-]{2,10})_?(?P<extra>.*)$"
+        )
 
     def _get_list_of_zimfarm_names(self):
         """Get and builds the list of zimfarm names"""
-        self.zimfarm_names = [
+        self.zimfarm_schedule_names = [
             item.get("name") for item in requests.get(self.url_api_zimfarm).json()
         ]
 
     def _get_list_of_library_names(self):
         """Get and builds the list of library names"""
         dom = parseString(requests.get(self.url_library_zim).content)
-        self.library_names = sorted(
+        self.books_in_library = sorted(
             [
                 (book.getAttribute("name"), book.getAttribute("url"))
                 for book in dom.getElementsByTagName("book")
@@ -40,19 +45,22 @@ class ReconciliationTitle:
         lang_codes = []
         try:
             lang_codes = name.split("_")[1].split("-")
-        except IndexError as exc:
-            print(exc)
-            self.library_names_not_proper.append([name, url])
+        except IndexError:
+            self.books_names_not_proper.append([name, url])
+            return
 
         for lang in lang_codes:
             try:
                 get_language_details(lang)
-            except NotFound as exc:
-                print(exc)
+            except NotFound:
                 self.code_languages_not_found.append([name, lang, url])
 
-        project, lang_codes, classifier = name.split("_")[:3]
-        return f"{project}_{lang_codes}_{classifier}"
+        m = self.name_reg.match(name)
+
+        if not m:
+            self.books_names_not_proper.append([name, url])
+            return
+        return "{project}_{lang_codes}_{extra}".format(**m.groupdict())
 
     def _comparison_of_names(self):
         """
@@ -60,26 +68,21 @@ class ReconciliationTitle:
 
         """
 
-        self.zimfarm_names_not_in_library = self.zimfarm_names.copy()
-        for name, url in self.library_names:
+        for name, url in self.books_in_library:
             if len(name.split("_")) not in [3, 4]:
-                self.library_names_not_proper.append([name, url])
+                self.books_names_not_proper.append([name, url])
                 continue
 
             ident = self.get_ident_from_name(name, url)
             self.data_ident.append([name, ident, url])
-            if ident in self.zimfarm_names:
-                self.ident_in_zimfarm.append([ident, url])
-                try:
-                    self.zimfarm_names_not_in_library.remove(ident)
-                except ValueError as exc:
-                    print(f"Name is not remove {exc}")
+            if ident in self.zimfarm_schedule_names:
+                self.ident_from_name_found_in_zimfarm.append([ident, url])
             else:
-                self.ident_not_in_library_names.append([name, url])
+                self.ident_from_name_not_found_in_zimfarm.append([name, url])
 
     def write_to_csv(self, filename, header, data):
         print(f"Write {filename} file to csv")
-        with open(f"scripts/{filename}", "w", newline="") as csvfile:
+        with open(f"{filename}", "w", newline="") as csvfile:
             write = csv.writer(csvfile)
             write.writerows([header])
             write.writerows(data)
@@ -87,27 +90,21 @@ class ReconciliationTitle:
     def _dump(self):
         # Table of idents and book names form library_zim
         self.write_to_csv(
-            "library_zim_names.csv",
+            "idents_and_book_names_form_library_zim.csv",
             ["Name", "Ident", "url"],
             self.data_ident,
         )
-        # Table of code language error
+        # Table of code languages not found
         self.write_to_csv(
             "code_languages_not_found.csv",
             ["Name", "code", "url"],
             self.code_languages_not_found,
         )
-        # Table of library_names not proper
+        # Table of Book names which failed to gen a valid ident
         self.write_to_csv(
-            "library_names_not_proper.csv",
+            "book_names_which_failed_to_gen_a_valid_ident.csv",
             ["Name", "url"],
-            self.library_names_not_proper,
-        )
-        # Table of zimfarm names not in library
-        self.write_to_csv(
-            "zimfarm_names_not_in_library.csv",
-            ["name"],
-            [[item] for item in self.zimfarm_names_not_in_library],
+            self.books_names_not_proper,
         )
 
     def run(self):
@@ -115,14 +112,30 @@ class ReconciliationTitle:
         self._get_list_of_library_names()
         self._comparison_of_names()
         self._dump()
-        print(
-            "Table of stats\n"
-            f"nb ident in zimfarm: {len(self.ident_in_zimfarm)}\n"
-            f"nb ident not in library_names: {len(self.ident_not_in_library_names)}\n"
-            f"nb zimfarm names: {len(self.zimfarm_names)}\n"
-            f"nb library names: {len(self.library_names)}\n"
-            f"nb library names not proper: {len(self.library_names_not_proper)}\n"
-            f"nb zimfarm names not in library: {len(self.zimfarm_names_not_in_library)}"
+
+        info("Table of stats")
+        data_ident_builder = TableBuilderClassic()
+        data_ident_builder.build_table(
+            header=["Name", "count"],
+            data=[
+                [
+                    "Nb. of ident-from-name found in Zimfarm: ",
+                    f"{len(self.ident_from_name_found_in_zimfarm)}",
+                ],
+                [
+                    "Nb. of ident-from-name NOT found in Zimfarm: ",
+                    f"{len(self.ident_from_name_not_found_in_zimfarm)}",
+                ],
+                [
+                    "Nb. of Zimfarm schedule names: ",
+                    f"{len(self.zimfarm_schedule_names)}",
+                ],
+                ["Nb. of Books in Library: ", f"{len(self.books_in_library)}"],
+                [
+                    "Nb. of Book names which failed to gen a valid ident: ",
+                    f"{len(self.books_names_not_proper)}",
+                ],
+            ],
         )
 
 
