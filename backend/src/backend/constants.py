95723f96facded6c21fe816e466a5905cca43218
Fadiga Ibrahima <ibfadiga@gmail.com>
2022-07-11 17:43:40 +0000

    Fixed #71: Auto deleting books

--- backend/src/backend/constants.py
+++ backend/src/backend/constants.py
@@ -1,12 +1,22 @@
+import logging
 import os
 import pathlib
 from dataclasses import dataclass
 
+from zimscraperlib.logging import getLogger as lib_getLogger
+
 src_dir = pathlib.Path(__file__).parent.resolve()
+
+logger = lib_getLogger(
+    src_dir.name,
+    level=logging.INFO,
+    log_format="[%(asctime)s] %(levelname)s:%(message)s",
+)
 
 
 @dataclass
 class BackendConf:
+    nb_book_versions_to_keep = os.getenv("NB_BOOK_VERSIONS_TO_KEEP", 2)
     database_url: str = os.getenv("DATABASE_URL", f"sqlite:////{src_dir}/dev.db")
     allowed_origins = os.getenv(
         "ALLOWED_ORIGINS",
